4fe9264dd5f0be9f78aa41e89278fd09d78e8109
jyotirmoydeka <68185650+jyotirmoydeka@users.noreply.github.com>
2022-12-23 15:13:11 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo...."
+git clone https://github.com/jyotirmoydeka/MdiskSearchBotV2.git /MdiskSearchBotV2
+cd /MdiskSearchBotV2
+pip3 install -r requirements.txt
+echo "Starting Bot...."
+python3 main.py
